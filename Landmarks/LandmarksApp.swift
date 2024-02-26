4b3100d5fcab68a8298ecf6dde8c4be95d023538
Darian <xxy.moming@gmail.com>
2024-02-26 12:17:07 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Darian Mitchell on 2024/2/26.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
